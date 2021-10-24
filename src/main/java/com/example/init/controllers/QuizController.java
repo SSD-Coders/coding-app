119efcbf9c40d72236e2291cd702c7d9a3b14484
Jamal Eldeen Wari <jamalwari2@gmail.com>
2021-10-24 23:55:42 +0300

    adding some stuff to github

--- src/main/java/com/example/init/controllers/QuizController.java
+++ src/main/java/com/example/init/controllers/QuizController.java
@@ -1,21 +1,24 @@
 package com.example.init.controllers;
 
 import com.example.init.models.Quiz;
+import com.example.init.models.QuizDTO;
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import org.json.simple.JSONObject;
+import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class QuizController {
@@ -24,18 +27,70 @@ public class QuizController {
 
 
     @GetMapping("/quiz")
-    public String getQuiz(Model model) throws IOException {
+    public String getQuiz(Model model) throws IOException, ParseException {
         Gson gson = new Gson();
         String urlApi = "https://quizapi.io/api/v1/questions?apiKey=phkL2Z69NDCrImLlfnLoElQkoF3StoJlQrAeKvkf&limit=10&css";
         HttpURLConnection connection = (HttpURLConnection) new URL(urlApi).openConnection();
         InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream());
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+//        String data = bufferedReader.readLine();
         String data = bufferedReader.readLine();
         Type jsonCasting = new TypeToken<List<Quiz>>(){}.getType();
         List<Quiz> jsonList  = gson.fromJson(data,jsonCasting);
+        List<QuizDTO> fromJson  = gson.fromJson(data,jsonCasting);
+
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("answers" , data);
+        model.addAttribute("answers" , jsonObject);
+
+//        List<Quiz> jsonMap  = Arrays.asList(gson.fromJson(data,Quiz[].class));
+
+
+//        JSONParser jsonParser = new JSONParser();
+//        Object ob  = jsonParser.parse(data);
+//        JSONObject jsonObject = (JSONObject) ob{0};
+//        String answers = (String) jsonObject.get("answers");
+//
+//
+//        List<QuizDTO> jsonAnswers  = Arrays.asList(gson.fromJson(answers,QuizDTO[].class));
+
         model.addAttribute("quiz",jsonList);
-//        model.addAttribute("answers",jsonList);
+        System.out.println(jsonList.toString());
+        QuizDTO quizDTO = new QuizDTO();
+//        System.out.println(data+" "+ jsonMap);
+//        List<QuizDTO> jsonMap  = Arrays.asList(gson.fromJson(data,QuizDTO[].class));
+//        JSONObject jsonObject = new JSONObject();
+//
+//        Map ans = new LinkedHashMap(6);
+//        ans.put("answer_a",quizDTO.getAnswer_a());
+//        ans.put("answer_b",quizDTO.getAnswer_b());
+//        ans.put("answer_c",quizDTO.getAnswer_c());
+//        ans.put("answer_d",quizDTO.getAnswer_d());
+//        ans.put("answer_e",quizDTO.getAnswer_e());
+//        ans.put("answer_f",quizDTO.getAnswer_f());
+//        jsonObject.toJSONString(jsonList)
+//        jsonObject.put("answers",ans);
+//        model.addAttribute("answers",jsonAnswers);
         bufferedReader.close();
         return "quiz";
     }
+//    @GetMapping("/quizAns")
+//    public String fetchAns(Model model){
+//        QuizDTO quizDTO = new QuizDTO();
+//        JsonObject jsonObject = new JsonObject();
+//        Map ans = new LinkedHashMap(6);
+//        ans.put("answer_a",quizDTO.getAnswer_a());
+//        ans.put("answer_b",quizDTO.getAnswer_b());
+//        ans.put("answer_c",quizDTO.getAnswer_c());
+//        ans.put("answer_d",quizDTO.getAnswer_d());
+//        ans.put("answer_e",quizDTO.getAnswer_e());
+//        ans.put("answer_f",quizDTO.getAnswer_f());
+//        jsonObject.add("answers", (JsonElement) ans);
+//        model.addAttribute("answers",jsonObject);
+//        return "quiz";
+//
+//    }
+
+
 }
--- src/main/java/com/example/init/models/Quiz.java
+++ src/main/java/com/example/init/models/Quiz.java
@@ -8,7 +8,9 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Entity
 public class Quiz {
@@ -17,13 +19,36 @@ public class Quiz {
     @Column(name = "id", nullable = false)
     private Long id;
 
+//    public String answers;
     public String question;
 //    public String answers;
+    public String answer_a;
+    public String answer_b;
+    public String answer_c;
+    public String answer_d;
+    public String answer_e;
+    public String answer_f;
 
-    public Quiz(String question) {
+//    public String getAnswers() {
+//        return answers;
+//    }
+//
+//    public void setAnswers(String answers) {
+//        this.answers = answers;
+//    }
+
+    public Quiz(String question, String answer_a, String answer_b, String answer_c, String answer_d, String answer_e, String answer_f) {
         this.question = question;
 //        this.answers = answers;
+        this.answer_a = answer_a;
+        this.answer_b = answer_b;
+        this.answer_c = answer_c;
+        this.answer_d = answer_d;
+        this.answer_e = answer_e;
+        this.answer_f = answer_f;
     }
+
+
 
     public Long getId() {
         return id;
