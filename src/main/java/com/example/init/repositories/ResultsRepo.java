4764534a8f9a0bd46c4fe62b7c3b5187d8db26b5
abdelqader-alomari <aboud.coding@gmail.com>
2021-10-28 16:50:11 +0300

    final updates

--- src/main/java/com/example/init/repositories/ResultsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.init.repositories;
-
-import com.example.init.models.ResultsQuiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResultsRepo extends JpaRepository<ResultsQuiz, Integer> {
-}
